54a4b8ae09948d19b9ae091ecc9e8fcf28c5b6a6
BALAKRISHNA SUGGU <balakrishna.suggu@ikea.com>
2018-03-23 13:48:12 +0100

    changed the text in Startup.cs

--- Dev/toDo1/toDo1/Startup.cs
+++ Dev/toDo1/toDo1/Startup.cs
@@ -27,7 +27,7 @@ namespace toDo1
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Its from asp.net");
             });
         }
     }
